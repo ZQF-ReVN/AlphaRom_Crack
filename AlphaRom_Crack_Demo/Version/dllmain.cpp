1a23a4cfe666fea2294655ea280c382654f6f24a
Dir-A <79064817+Dir-A@users.noreply.github.com>
2022-07-24 21:48:26 +0800

    Add files via upload

---
+++ AlphaRom_Crack_Demo/Version/dllmain.cpp
@@ -0,0 +1,59 @@
+#include "Hijack.h"
+#include <detours.h>
+
+HMODULE exeHmodule;
+
+typedef LPVOID(WINAPI* pVirtualAlloc)(
+	LPVOID lpAddress,
+	SIZE_T dwSize,
+	DWORD  flAllocationType,
+	DWORD  flProtec
+	);
+pVirtualAlloc rawVirtualAlloc;
+
+LPVOID WINAPI newVirtualAlloc(LPVOID lpAddress, SIZE_T dwSize, DWORD  flAllocationType, DWORD  flProtec)
+{
+	if (dwSize == 0x00572000) //alloc size of sarcheck.dll
+	{
+		if (*(DWORD*)0x00AFB25D == 0x5004EC83) // check entrypoint
+		{
+			*(DWORD*)0x00AFB25D = 0x90000CC2; // entrypoint => ret 0xC
+			DetourTransactionBegin();
+			DetourDetach(&(PVOID&)rawVirtualAlloc, newVirtualAlloc);
+			DetourTransactionCommit();
+		}
+	}
+
+	return rawVirtualAlloc(lpAddress, dwSize, flAllocationType, flProtec);
+}
+
+VOID StartHook()
+{
+	exeHmodule = GetModuleHandleW(NULL);
+	rawVirtualAlloc = VirtualAlloc;
+
+	DetourRestoreAfterWith();
+	DetourTransactionBegin();
+	DetourUpdateThread(GetCurrentThread());
+	DetourAttach(&(PVOID&)rawVirtualAlloc, newVirtualAlloc);
+	DetourTransactionCommit();
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		StartHook();
+		CreateHijack();
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		FreeHijack();
+		break;
+	}
+	return TRUE;
+}
